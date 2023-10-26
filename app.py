6a1badc357a1743dee195a4496ab1614a65548aa
Chandra Prakash Meghwal <114313891+chandra-prakash-meghwal@users.noreply.github.com>
2023-10-26 10:07:35 +0530

    Update app.py added mysql db create user model

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configure the database connection using environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@localhost/{os.environ['DB_NAME']}"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
 
 @app.route('/')
 def hello():
     return "Hello, Flask in Docker!"
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', port=5000)
